5d94765b2ac297c6f400d8d7a8c274e029e91cef
Paul Reed <paul.reed@puppet.com>
2025-03-27 21:23:45 -0400

    Add package installation

---
+++ manifests/init.pp
@@ -0,0 +1,23 @@
+# @summary A short summary of the purpose of this class
+#
+# A description of what this class does
+#
+# @example
+#   include nessus_essentials
+# 
+# @param version
+# @param architecture
+# @param base_url
+class nessus_essentials (
+  String $version      = '10.8.3',
+  String $architecture = $facts['os']['architecture'],
+  String $base_url     = 'https://www.tenable.com/downloads/api/v2/pages/nessus/files/Nessus',
+) {
+  if $facts['os']['family'] == 'RedHat' {
+    $package = "${base_url}-${version}-el${facts['os']['release']['major']}.${architecture}.rpm"
+  } elsif $facts['os']['name'] == 'Ubuntu' {
+    $package = "${base_url}-${version}-ubuntu1604.${architecture}.deb"
+  }
+
+  ensure_resource('package',$package, { 'ensure' => present, })
+}
